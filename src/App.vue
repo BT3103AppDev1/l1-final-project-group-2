73d354725fd94bb3f5aa4a446288f7f0635da5e2
Koh Zhe Wei <98997988+koh-zhe-wei@users.noreply.github.com>
2023-03-19 00:16:47 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
   <div id="nav" v-if="$store.state.user">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/leave">Leave</router-link>
+    <router-link to="/leave">Leave</router-link> |
+    <router-link to="/chat">Chat</router-link>
     <button @click="$store.dispatch('logout')">Logout</button>
   </div>
   <router-view/>
