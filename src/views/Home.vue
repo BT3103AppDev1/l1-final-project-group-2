652e854d1316d6277d237c23e028d1a26c10c337
evewzhiru16 <evewzhiru@gmail.com>
2023-03-19 13:54:00 +0800

    Added Home Task Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,30 @@
 <template>
-    <main class="home">
-      <h1>Home</h1>
-      <h2> homie</h2>
-    </main>
-  </template>
+  <main class="home">
+    <PersonalPage @added="change" :key="refreshComp"/>
+
+    
+  </main>
+</template>
+
+<script>
+import PersonalPage from '@/components/PersonalPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PersonalPage
+  },
+
+  data() {
+    return{
+      refreshComp:0
+    }
+  },
+
+  methods: {
+    change(){
+      this.refreshComp += 1
+    }
+  }
+}
+</script>
