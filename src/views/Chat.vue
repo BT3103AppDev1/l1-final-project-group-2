fd53f1e80bd4a92642b6099676f8ff51a9ece451
Koh Zhe Wei <98997988+koh-zhe-wei@users.noreply.github.com>
2023-04-01 22:32:40 +0800

    Update Chat.vue (Syncs with firebase but past messages not appearing)

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,29 +1,29 @@
 <template>
-  <div class="chat-container">
-    <div v-for="conversation in conversations" :key="conversation.id" class="conversation">
-      <h3>{{ conversation.displayName }}</h3>
-      <div v-for="message in conversation.messages" :key="message.id" class="message">
-        <div>{{ message.senderId }}: {{ message.content }}</div>
+  <div>
+    <div v-if="selectedRecipientMessages.length > 0">
+      <div v-for="(message, index) in selectedRecipientMessages" :key="index">
+        <div>
+          {{ message.senderId }}: {{ message.content }}
+          ({{ new Date(message.timestamp.seconds * 1000).toLocaleString() }})
+        </div>
       </div>
     </div>
-    <div class="user-select">
-      <label for="userSelect">Select user to send a message:</label>
-      <select id="userSelect" v-model="selectedUser">
+    <div>
+      <label for="userSelect">Select user to send a message: </label>
+      <select id="userSelect" v-model="selectedUserId">
         <option disabled value="">Please select a user</option>
         <option v-for="user in users" :key="user.id" :value="user.id">{{ user.name }}</option>
       </select>
     </div>
-    <form @submit.prevent="sendMessage" class="message-form">
-      <input type="text" v-model="newMessage" class="message-input" />
-      <button type="submit" :disabled="!selectedUser" class="send-button">Send</button>
+    <form @submit.prevent="sendMessage">
+      <input type="text" v-model="newMessage" />
+      <button type="submit" :disabled="!selectedUserId">Send</button>
     </form>
   </div>
 </template>
 
-
 <script>
-import { getAuth } from "firebase/auth";
-import { getFirestore, collection, doc, getDocs, getDoc, setDoc, query, orderBy, where } from "firebase/firestore";
+import { collection, doc, getDocs, getDoc, setDoc, query, orderBy, where } from "firebase/firestore";
 import { auth, db } from '../firebase/firebase';
 
 export default {
@@ -31,20 +31,42 @@ export default {
     return {
       newMessage: "",
       conversations: [],
-      selectedUser: "",
+      selectedUserId: "",
       users: [],
+      selectedConversation: null,
+      selectedRecipientMessages: [],
     };
   },
+  computed: {
+  selectedConversation() {
+    return this.conversations.find(conversation => {
+      return conversation.participants.includes(this.selectedUser);
+    });
+  },
+},
+
+
   async created() {
+    const loggedInUser = auth.currentUser;
+
+    console.log("Logged-in user:", loggedInUser);
+
     await this.fetchAndInitializeConversations();
     await this.fetchUsers();
   },
+  watch: {
+  selectedUser(newValue) {
+    this.selectedConversation = this.conversations.find(conversation => {
+      return conversation.participants.includes(newValue);
+    });
+  },
+},
+
+
   methods: {
     async fetchUsers() {
       const loggedInUserId = auth.currentUser.uid;
       const usersSnapshot = await getDocs(collection(db, "users"));
-
-      console.log("Fetched users:", usersSnapshot.docs); // Add this line to debug
 
       usersSnapshot.forEach((userDoc) => {
         if (userDoc.id !== loggedInUserId) {
@@ -57,72 +79,75 @@ export default {
     },
 
     async fetchAndInitializeConversations() {
-  const loggedInUserId = auth.currentUser.uid;
-  const userConversationsSnapshot = await getDocs(
-    query(collection(db, "conversations"), where("participants", "array-contains", loggedInUserId))
-  );
+      const loggedInUserId = auth.currentUser.uid;
+      const userConversationsSnapshot = await getDocs(
+        query(collection(db, "conversations"), where("participants", "array-contains", loggedInUserId))
+      );
 
-  for (const conversationDoc of userConversationsSnapshot.docs) {
-    const participants = conversationDoc.data().participants;
-    const otherUserId = participants.find((id) => id !== loggedInUserId);
-    
-    console.log("otherUserId:", otherUserId); // Add this line to debug
-    
-    const otherUserDoc = await getDoc(doc(db, "users", otherUserId));
+      for (const conversationDoc of userConversationsSnapshot.docs) {
+        const participants = conversationDoc.data().participants;
+        const otherUserId = participants.find((id) => id !== loggedInUserId);
 
-    console.log("otherUserDoc:", otherUserDoc); // Add this line to debug
-    console.log("otherUserDoc.data():", otherUserDoc.data()); // Add this line to debug
+        const otherUserDoc = await getDoc(doc(db, "users", otherUserId));
 
-    this.conversations.push({
-      id: conversationDoc.id,
-      name: otherUserDoc.data().name,
-      messages: await this.fetchMessages(conversationDoc.id),
-    });
-  }
-},
+        const messages = await this.fetchMessages(conversationDoc.id);
+
+        this.conversations.push({
+          id: conversationDoc.id,
+          displayName: otherUserDoc.data().name,
+          messages: messages,
+        });
+      }
+    },
 
     async fetchMessages(conversationId) {
       const messagesSnapshot = await getDocs(
         query(collection(doc(db, "conversations", conversationId), "messages"), orderBy("timestamp", "asc"))
       );
-      return messagesSnapshot.docs.map((doc) => doc.data());
+      const messages = messagesSnapshot.docs.map((doc) => doc.data());
+
+      return messages;
     },
+
     async sendMessage() {
-  if (!this.selectedUser || !this.newMessage.trim()) {
-    return;
-  }
+      if (!this.selectedUserId || !this.newMessage.trim()) {
+        return;
+      }
 
-  const loggedInUserId = auth.currentUser.uid;
-  const conversationId = this.getConversationId(loggedInUserId, this.selectedUser);
-  const message = {
-    id: `${Date.now()}`, // Add this line to create a unique id for each message
-    content: this.newMessage,
-    senderId: loggedInUserId,
-    timestamp: new Date(),
-  };
+      const loggedInUserId = auth.currentUser.uid;
+      const conversationId = this.getConversationId(loggedInUserId, this.selectedUserId);
+      const message = {
+        content: this.newMessage,
+        senderId: loggedInUserId,
+        timestamp: new Date(),
+      };
 
-  const conversationRef = doc(db, "conversations", conversationId);
-  const conversationSnapshot = await getDoc(conversationRef);
+      const conversationRef = doc(db, "conversations", conversationId);
+      const conversationSnapshot = await getDoc(conversationRef);
 
-  if (conversationSnapshot.exists()) {
-    await setDoc(conversationRef, {
-      participants: conversationSnapshot.data().participants,
-      messages: [...conversationSnapshot.data().messages, message],
-    });
-  } else {
-    await setDoc(conversationRef, {
-      participants: [loggedInUserId, this.selectedUser],
-      messages: [message],
-    });
-  }
+      if (conversationSnapshot.exists()) {
+        await setDoc(conversationRef, {
+          participants: conversationSnapshot.data().participants,
+          messages: [...conversationSnapshot.data().messages, message],
+        });
+      } else {
+        await setDoc(conversationRef, {
+          participants: [loggedInUserId, this.selectedUserId],
+          messages: [message],
+        });
+      }
 
-  this.newMessage = '';
-},
-
+      this.newMessage = '';
+    },
 
     getConversationId(user1, user2) {
       const ids = [user1, user2].sort();
       return `${ids[0]}_${ids[1]}`;
+    },
+
+    formatDate(timestamp) {
+      const date = new Date(timestamp.toDate());
+      return date.toLocaleString();
     },
   },
 };
@@ -132,64 +157,51 @@ export default {
 .chat-container {
   max-width: 800px;
   margin: 0 auto;
-  padding: 20px;
-}
-
-.conversation {
-  background-color: #f2f2f2;
-  border-radius: 5px;
-  padding: 20px;
-  margin-bottom: 20px;
+  padding: 1rem;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  background-color: #f8f8f8;
+  border-radius: 4px;
 }
 
 .message {
-  margin-bottom: 10px;
-}
-
-.user-select {
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 20px;
-}
-
-#userSelect {
-  margin-top: 10px;
-  padding: 5px 10px;
-  font-size: 16px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
-
-.message-form {
-  display: flex;
-  justify-content: space-between;
-}
-
-.message-input {
-  flex-grow: 1;
-  padding: 10px;
-  margin-right: 10px;
-  font-size: 16px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
+  padding: 0.5rem;
+  margin-bottom: 0.5rem;
   background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
-.send-button {
-  background-color: #4CAF50;
+.timestamp {
+  font-size: 0.8rem;
+  color: #777;
+  margin-left: 1rem;
+}
+
+form {
+  display: flex;
+  margin-top: 1rem;
+}
+
+input[type="text"] {
+  flex: 1;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button[type="submit"] {
+  margin-left: 0.5rem;
+  padding: 0.5rem 1rem;
+  background-color: #007bff;
+  color: #fff;
   border: none;
-  color: white;
-  padding: 10px 20px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  border-radius: 5px;
+  border-radius: 4px;
   cursor: pointer;
 }
 
-.send-button:disabled {
+button[type="submit"]:disabled {
   background-color: #ccc;
   cursor: not-allowed;
 }
 </style>
+
